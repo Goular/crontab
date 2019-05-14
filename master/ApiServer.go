05327c7e1460b883ef31af3a214743d4d0647698
goular <zhaojt_cs@126.com>
2019-05-14 17:40:40 +0800

    提交Api Server的服务到相关接口上

---
+++ master/ApiServer.go
@@ -0,0 +1,56 @@
+package master
+
+import (
+	"net"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+var (
+	// 单例对象
+	G_apiServer *ApiServer
+)
+
+// 任务的HTTP接口
+type ApiServer struct {
+	httpServer *http.Server
+}
+
+// 保存任务接口
+func handleJobSave(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// 初始化服务
+func InitApiServer() (err error) {
+	var (
+		mux        *http.ServeMux
+		listener   net.Listener
+		httpServer *http.Server
+	)
+	// 配置路由
+	mux = http.NewServeMux()
+	mux.HandleFunc("/job/save", handleJobSave)
+
+	// 启动TCP监听
+	if listener, err = net.Listen("tcp", ":"+strconv.Itoa(G_config.ApiPort)); err != nil {
+		return
+	}
+
+	// 创建一个HTTP服务
+	httpServer = &http.Server{
+		ReadTimeout:  time.Duration(G_config.ApiReadTimeout) * time.Millisecond,
+		WriteTimeout: time.Duration(G_config.ApiWriteTimeout) * time.Millisecond,
+		Handler:      mux,
+	}
+
+	G_apiServer = &ApiServer{
+		httpServer: httpServer,
+	}
+
+	// 开启异步协程HTTP服务
+	go httpServer.Serve(listener)
+
+	return
+}
---
+++ master/main/master.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/Goular/crontab/master"
+	"runtime"
+)
+
+var (
+	confFile string // 配置文件路径
+)
+
+// 解析命令行参数
+func initArgs() {
+	// 支持运行指令: master -config ./master.json
+	flag.StringVar(&confFile, "config", "./master.json", "指定master.json文件")
+	flag.Parse()
+}
+
+// 初始化协程数量
+func initEnv() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+}
+
+var (
+	err error
+)
+
+func main() {
+	// 初始化命令行参数
+	initArgs()
+
+	// 初始化协程
+	initEnv()
+
+	// 加载配置
+	if err = master.InitConfig(confFile); err != nil {
+		goto ERR
+	}
+
+	// 启动HTTP API服务
+	if err = master.InitApiServer(); err != nil {
+		goto ERR
+	}
+
+	// 正常退出
+	return
+ERR:
+	fmt.Println(err)
+}

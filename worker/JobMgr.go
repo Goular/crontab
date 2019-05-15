612766ac94b82a563b8a59312b37eab75702736c
goular <zhaojt_cs@126.com>
2019-05-15 17:31:10 +0800

    监听etcd中任务变化

---
+++ worker/JobMgr.go
@@ -0,0 +1,124 @@
+package worker
+
+import (
+	"context"
+	"fmt"
+	"github.com/Goular/crontab/common"
+	"go.etcd.io/etcd/clientv3"
+	"go.etcd.io/etcd/mvcc/mvccpb"
+	"time"
+)
+
+// 任务管理器
+type JobMgr struct {
+	client  *clientv3.Client
+	kv      clientv3.KV
+	lease   clientv3.Lease
+	watcher clientv3.Watcher
+}
+
+var (
+	// 单例
+	G_jobMgr *JobMgr
+)
+
+func InitJobMgr() (err error) {
+	var (
+		client  *clientv3.Client
+		config  clientv3.Config
+		kv      clientv3.KV
+		lease   clientv3.Lease
+		watcher clientv3.Watcher
+	)
+	// 初始化配置
+	config = clientv3.Config{
+		Endpoints:   G_config.EtcdEndpoints,
+		DialTimeout: time.Duration(G_config.EtcdDialTimeout) * time.Millisecond,
+	}
+
+	// 建立连接
+	if client, err = clientv3.New(config); err != nil {
+		// 创建连接失败
+		fmt.Println(err)
+		return
+	}
+
+	// 得到KV和Lease的API子集
+	kv = clientv3.NewKV(client)
+	lease = clientv3.NewLease(client)
+	watcher = clientv3.NewWatcher(client)
+
+	// 赋值单例
+	G_jobMgr = &JobMgr{
+		client:  client,
+		kv:      kv,
+		lease:   lease,
+		watcher: watcher,
+	}
+
+	// 启动任务监听
+	G_jobMgr.watchJobs()
+
+	return
+}
+
+// 监听任务的变化
+func (JobMgr *JobMgr) watchJobs() (err error) {
+	var (
+		getResp            *clientv3.GetResponse
+		kvpair             *mvccpb.KeyValue
+		job                *common.Job
+		watchStartRevision int64
+		watchChan          clientv3.WatchChan
+		watchResp          clientv3.WatchResponse
+		watchEvent         *clientv3.Event
+		jobName            string
+		jobEvent           *common.JobEvent
+	)
+
+	// 1. get一下/cron/jobs/目录下的所有任务，并且获知当前集群的revision
+	if getResp, err = JobMgr.kv.Get(context.TODO(), common.JOB_SAVE_DIR, clientv3.WithPrefix()); err != nil {
+		return
+	}
+
+	// 当前有哪些任务
+	for _, kvpair = range getResp.Kvs {
+		if job, err = common.UnpackJob(kvpair.Value); err == nil {
+			jobEvent = common.BuildJobEvent(common.JOB_EVENT_SAVE, job)
+			// TODO:把这个job同步给scheduler(调度协程)
+		}
+	}
+
+	// 2.从该revision向后监听变化事件
+	go func() { // 监听协程
+		// 从GET时刻的后续版本开始监听变化
+		watchStartRevision = getResp.Header.Revision + 1
+		// 监听 /cron/jobs目录的后续变化
+		watchChan = JobMgr.watcher.Watch(context.TODO(), common.JOB_SAVE_DIR, clientv3.WithRev(watchStartRevision))
+		// 处理监听事件
+		for watchResp = range watchChan {
+			// 每一次遍历过来的，可能不止一种事件
+			for watchEvent = range watchResp.Events {
+				switch watchEvent.Type {
+				case mvccpb.PUT: //任务保存事件
+					if job, err = common.UnpackJob(watchEvent.Kv.Value); err != nil {
+						continue
+					}
+					// 构造一个更新Event
+					jobEvent = common.BuildJobEvent(common.JOB_EVENT_SAVE, job)
+				case mvccpb.DELETE: //任务被删除了
+					// Delete /cron/jobs/job10
+					jobName = common.ExtractJobName(string(watchEvent.Kv.Key))
+
+					job = &common.Job{Name: jobName}
+
+					// 构建一个删除的Event
+					jobEvent = common.BuildJobEvent(common.JOB_EVENT_DELETE, job)
+				}
+				// TODO:推送给scheduler
+				//G_Sccheduler.PushJobEvent(jobEvent)
+			}
+		}
+	}()
+	return
+}
---
+++ worker/main/worker.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/Goular/crontab/master"
+	"github.com/Goular/crontab/worker"
+	"runtime"
+)
+
+var (
+	confFile string // 配置文件路径
+)
+
+// 解析命令行参数
+func initArgs() {
+	// 支持运行指令: worker -config ./master.json
+	flag.StringVar(&confFile, "config", "./worker.json", "指定master.json文件")
+	flag.Parse()
+}
+
+// 初始化协程数量
+func initEnv() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+}
+
+var (
+	err error
+)
+
+func main() {
+	// 初始化命令行参数
+	initArgs()
+
+	// 初始化协程
+	initEnv()
+
+	// 加载配置
+	if err = master.InitConfig(confFile); err != nil {
+		goto ERR
+	}
+
+	// 初始化任务管理器
+	if err = worker.InitJobMgr(); err != nil {
+		goto ERR
+	}
+
+	// 正常退出
+	select {}
+
+	return
+ERR:
+	fmt.Println(err)
+}

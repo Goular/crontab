a55eb1a82e784e0225aa09cd2a51995aa481af45
goular <zhaojt_cs@126.com>
2019-05-16 17:22:15 +0800

    修改bug

--- worker/Executor.go
+++ worker/Executor.go
@@ -58,10 +58,9 @@ func (executor *Executor) ExecuteJob(info *common.JobExecuteInfo) {
 			result.EndTime = time.Now()
 			result.Output = output
 			result.Err = err
-
-			// 任务执行完成后，把执行的结果返回给Scheduler，Scheduler会从executingTable中删除掉执行的记录
-			G_scheduler.PushJobResult(result)
 		}
+		// 任务执行完成后，把执行的结果返回给Scheduler，Scheduler会从executingTable中删除掉执行的记录
+		G_scheduler.PushJobResult(result)
 	}()
 }
 
